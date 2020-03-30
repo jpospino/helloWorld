95a4c8d1a0b3d19cf34b6e0235fede7f2d9b4169
Juan Pablo Ospino Solano <jp.ospino@uniandes.edu.co>
2020-03-30 16:31:36 -0500

    sin eureka en el mismo servicio

--- src/main/java/com/jospino/springboot/app/hello/world/SpringbootServicioHelloWorldApplication.java
+++ src/main/java/com/jospino/springboot/app/hello/world/SpringbootServicioHelloWorldApplication.java
@@ -3,9 +3,7 @@ package com.jospino.springboot.app.hello.world;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioHelloWorldApplication {
